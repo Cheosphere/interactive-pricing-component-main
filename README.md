a879d96eb65d4a7fed487656e4e303e684619b26
Eliseo Avila <ebae82@hotmail.com>
2022-10-20 20:17:04 -0300

    Cargando proyecto en repositorio remoto

---
+++ README.md
@@ -0,0 +1,47 @@
+# Componente de precios interactivo
+
+El desafío es desarrollar este componente de precios y lograr que se parezca lo más posible al diseño.
+
+- HTML
+- CSS
+- JS Vanilla (with animejs library)
+
+![desktop-design](https://user-images.githubusercontent.com/112582420/197022446-abe6e03d-1328-4c19-af6b-7d0868bb5fe7.jpg)
+
+# Guía de Estilo
+
+## Diseño
+
+Los diseños se crearon con los siguientes anchos:
+
+- Mobile: 375px
+- Desktop: 1440px
+
+## Colores
+### Primarios
+
+- Soft Cyan (Full Slider Bar): hsl(174, 77%, 80%)
+- Strong Cyan (Slider Backround): hsl(174, 86%, 45%)
+- Light Grayish Red (Discount Background): hsl(14, 92%, 95%)
+- Light Red (Discount Text): hsl(15, 100%, 70%)
+- Pale Blue (CTA Text): hsl(226, 100%, 87%)
+
+### Neutrales
+
+- White (Pricing Component Background): hsl (0, 0%, 100%)
+- Very Pale Blue (Main Background): hsl(230, 100%, 99%)
+- Light Grayish Blue (Empty Slider Bar): hsl(224, 65%, 95%)
+- Light Grayish Blue (Toggle Background): hsl(223, 50%, 87%)
+- Grayish Blue (Text): hsl(225, 20%, 60%)
+- Dark Desaturated Blue (Text & CTA Background): hsl(227, 35%, 25%)
+
+## Tipografía
+
+### Cuerpo
+
+- Font size (Párrafo Introductorio): 15px
+
+### Fuentes
+
+- Family: [Manrope](https://fonts.google.com/specimen/Manrope)
+- Weights: 600, 800
---
+++ main.js
@@ -0,0 +1,69 @@
+const rangeBar = document.getElementById('range_bar')
+const pageViews = document.getElementById('pageviews')
+const planPrice = document.getElementById('price')
+const periodSwitch = document.getElementById('period_switch')
+
+periodSwitch.addEventListener('click', () => {
+    periodSwitch.classList.toggle('discount')
+    discount()
+})
+
+function discount() {
+    (rangeBar.oninput = () => {
+        if (!periodSwitch.classList.contains('discount')) {
+            if (rangeBar.value === '0') {
+                pageViews.innerHTML = '10K PAGEVIEWS'
+                planPrice.innerHTML = '$8.00'
+            } else if (rangeBar.value === '25') {
+                pageViews.innerHTML = '50K PAGEVIEWS'
+                planPrice.innerHTML = '$12.00'
+            } else if (rangeBar.value === '50') {
+                pageViews.innerHTML = '100K PAGEVIEWS'
+                planPrice.innerHTML = '$16.00'
+            } else if (rangeBar.value === '75') {
+                pageViews.innerHTML = '500K PAGEVIEWS'
+                planPrice.innerHTML = '$24.00'
+            } else {
+                pageViews.innerHTML = '1M PAGEVIEWS'
+                planPrice.innerHTML = '$36.00'
+            }
+        } else {
+            if (rangeBar.value === '0') {
+                pageViews.innerHTML = '10K PAGEVIEWS'
+                planPrice.innerHTML = '$6.00'
+            } else if (rangeBar.value === '25') {
+                pageViews.innerHTML = '50K PAGEVIEWS'
+                planPrice.innerHTML = '$9.00'
+            } else if (rangeBar.value === '50') {
+                pageViews.innerHTML = '100K PAGEVIEWS'
+                planPrice.innerHTML = '$12.00'
+            } else if (rangeBar.value === '75') {
+                pageViews.innerHTML = '500K PAGEVIEWS'
+                planPrice.innerHTML = '$18.00'
+            } else {
+                pageViews.innerHTML = '1M PAGEVIEWS'
+                planPrice.innerHTML = '$27.00'
+            }
+        }
+
+        anime({
+            targets: '.card_heading_pageviews',
+            opacity: [0, 1],
+            translateY: [-20, 0],
+            easing: 'easeInOutExpo',
+            duration: 400
+        });
+
+        anime({
+            targets: '.card_heading_price_container',
+            opacity: [0, 1],
+            translateY: [20, 0],
+            easing: 'easeInOutExpo',
+            duration: 300,
+            delay: 100
+        });
+
+    })() // IIFE
+}
+
+discount()
